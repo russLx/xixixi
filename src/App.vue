75cfcdcd2dd1e30fd73061e023b7c6eb10964fe3
russLx <819587302@qq.com>
2022-01-13 20:44:42 +0800

    第一次

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    app
+    <div>
+      <van-cell title="选择单个日期" :value="date" @click="show = true" />
+      <van-calendar v-model="show" @confirm="onConfirm" />
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import Vue from 'vue'
+import { Calendar, Cell } from 'vant'
+Vue.use(Calendar)
+Vue.use(Cell)
+export default {
+  name: 'App',
+  data() {
+    return {
+      date: '',
+      show: false
+    }
+  },
+  methods: {
+    formatDate(date) {
+      return `${date.getMonth() + 1}/${date.getDate()}`
+    },
+    onConfirm(date) {
+      this.show = false
+      this.date = this.formatDate(date)
     }
   }
 }
+</script>
+
+<style lang="less">
 </style>
